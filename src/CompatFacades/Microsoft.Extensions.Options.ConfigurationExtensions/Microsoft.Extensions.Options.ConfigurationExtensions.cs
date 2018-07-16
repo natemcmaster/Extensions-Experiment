e5198ed479dfb44c6542ccd9da2ea9d6b3610a11
Nate McMaster <nate.mcmaster@microsoft.com>
2018-07-16 16:43:02 -0700

    Consolidate Microsoft.Extensions.Options.ConfigurationExtensions into Microsoft.Extensions.Options

---
+++ src/CompatFacades/Microsoft.Extensions.Options.ConfigurationExtensions/Microsoft.Extensions.Options.ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+[assembly: TypeForwardedTo(typeof(ConfigurationChangeTokenSource<>))]
+[assembly: TypeForwardedTo(typeof(ConfigureFromConfigurationOptions<>))]
+[assembly: TypeForwardedTo(typeof(NamedConfigureFromConfigurationOptions<>))]
+[assembly: TypeForwardedTo(typeof(OptionsBuilderConfigurationExtensions))]
+[assembly: TypeForwardedTo(typeof(OptionsConfigurationServiceCollectionExtensions))]
+
